c1778acf51b03d767e4597e4630741908afddd0a
Yassin <91887766+le2p2dev@users.noreply.github.com>
2022-03-03 19:16:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import mapboxgl from "mapbox-gl";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 
 import Map from "./Map";
 
