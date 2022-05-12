c6e7cbc02268ccf25e56e603df3e03233d68faa5
stef <stefssim@gmail.com>
2022-05-12 11:47:19 +0200

    profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,31 +1,59 @@
-import React, {useRef, useState } from "react";
+import React, { useState } from "react";
 import Avatar from '@mui/material/Avatar';
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
 import "./Profile.css"
-import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import LinkIcon from '@mui/icons-material/Link';
-import PasswordIcon from '@mui/icons-material/Password';
-import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import TextField from '@mui/material/TextField';
+
 
 const Profile = () => {
+
 
     const Input = styled('input')({
         display: 'none',
     });
 
     const [srcPath,setSrcPath] = useState("../assets/user.jpg");
+    const [modifyName,setModifyName] = useState(false);
+    const [modifyPassword,setModifyPassword] = useState(false);
+    const [userInfo,setUserInfo] = useState();
+    const [username,setUsername] = useState('stef');
+    const [currentPassword,setCurrentPassword]=useState('');
+    const [newPassword,setNewPassword]=useState('');
+    const [newPasswordConfirm,setNewPasswordConfirm]=useState('');
+    const [doPasswordsMatch,setDoPasswordsMatch] = useState(false);
+
+    const handleChangeCurrentPassword = (event) => {
+        setCurrentPassword(event.target.value);
+    }
+
+    const handleChangeNewPassowrd =(event) => {
+        setNewPassword(event.target.value);
+    }
+    const handleChangeNewPassowrdConfirm = (event) => {
+        setNewPasswordConfirm(event.target.value);
+    }
+
+    const checkPasswordChange = () => {
+
+        console.log(newPassword);
+        console.log(newPasswordConfirm);
+        if(newPassword == newPasswordConfirm){
+            setDoPasswordsMatch(true);
+            console.log("here");
+        }
+        else {
+            setDoPasswordsMatch(false);
+        }
+
+    }
+
 
     const  handleUploadBtnClick = async (event) => {
 
         const file = event.target.files[0];
-        console.log("file = ",file);
         const url = await(readURL(file));
-        console.log("url=",url)
         setSrcPath(url);
     }
 
@@ -37,21 +65,53 @@ const Profile = () => {
             reader.readAsDataURL(file);
         });
     };
+   
+    
+
+    const modifyBtn = (toModify,ev) => {
+    
+        
+        if ((toModify == "name") && (modifyName==false)){
+            setModifyName(true);
+        }
+        else if(modifyName==true){
+            //use mutate to change name
+        }
+        else if((toModify == "password") && (modifyPassword==false)){
+            setModifyPassword(true);
+        }
+        else if(modifyPassword==true){
+            checkPasswordChange();
+            console.log("do they?",doPasswordsMatch);
+            if(doPasswordsMatch==true){
+                //use mutate to change psw
+                console.log("oke");
+            }
+            else{
+                //show notification that passwords dont match
+                console.log("noke");
+            }
+        }
+        else return;
+    }
+
+    const onCancelClick = (toModify) => {
+
+        if (toModify == "name"){
+            setModifyName(false);
+        }
+        else if(toModify == "password"){
+            setModifyPassword(false);
+        }
+        else return;
+    }
 
     return(
-        <div id="profilePage">      
 
-            <Box  id="img">
-                <Grid 
-                    container   
-                    direction="column"
-                    justifyContent="center"
-                    alignItems="flex-end"
-                    rowSpacing={0} 
-                    columnSpacing={{ xs: 1, sm: 2, md: 2 }} 
-                >
-                    <Grid item xs={3} md={12}>
-                        <div id="avatarAndButton">
+        <div id = "page">
+            <div id="sidePannel">
+                <div id="img">
+                    <div id="avatarAndButton">
                         <Avatar
                             //alt='user avatar'
                             src={srcPath}
@@ -61,49 +121,68 @@ const Profile = () => {
                             <Input onChange = {handleUploadBtnClick}  accept="image/*" id="contained-button-file" multiple type="file" />
                             <Button variant="contained" component="span"> <AddPhotoAlternateOutlinedIcon/> </Button>
                         </label>      
-                        </div>     
-                    </Grid>
-                </Grid>
-            </Box>
-            
-            <Box id = "userInfo">
-
-                <div id="userNameTitle">
-                    <div id="name"> Stef Sim </div>
+                    </div>     
                 </div>
-                
-                <Grid 
-                    container  
-                    rowSpacing={0} 
-                    columnSpacing={{ xs: 1, sm: 2, md: 2 }}
-                    id="infoContainer">
-                    <Grid item xs={3} md={12}>
-                        <span className="spanBox">
-                            <LocationOnOutlinedIcon className="uIcons"/>
-                            <div className="uInfo"> Strasbourg,France </div>
-                        </span>
-                    </Grid>
-                    <Grid item xs={3} md={12}>
-                        <span className="spanBox">
-                            <LinkIcon className="uIcons"/>
-                            <div className="uInfo"> stefTwitter.com </div>
-                        </span>
-                    </Grid>
-                    <Grid item xs={3} md={12}>
-                        <span className="spanBox">
-                            <AlternateEmailIcon className="uIcons"/>
-                            <div  className="uInfo"> stefssim@gmail.com </div>
-                        </span>
-                        
-                    </Grid>
-                    <Grid item xs={3} md={12}>
-                        <span className="spanBox">
-                            <PasswordIcon className="uIcons"/>
-                            <div className="uInfo"> ******** </div>
-                        </span>
-                    </Grid>
-                </Grid>
-            </Box>
+            </div>
+
+            <div id = "infoPannel">
+                <header id="headerProfilePage">
+                    <h1> Your personal information</h1>
+                </header>
+                <div className = "infoBoxes">
+                    <div>
+                        <p> {username} </p>
+                        {
+                            (modifyName==false)? <p> info from back end  </p> :
+                            <div>
+                                 <TextField
+                                    label="Name"
+                                    type="text"
+                                />
+                                <Button onClick = {() => onCancelClick("name") }className = "modifyButton" variant="contained" >  Cancel  </Button>
+
+
+                            </div>
+                        }
+                    </div>
+                    <Button onClick = {(ev) => modifyBtn("name",ev)} className = "modifyButton" variant="contained" > {modifyName==false? "Modify" : "Save"}  </Button>
+
+                </div>
+                <div className = "infoBoxes">
+                    <div>
+                        <p> Password </p>
+                        {
+                            (modifyPassword==false)? <p> ********  </p> :
+                            <div id = "passwordChange">
+                                <TextField
+                                    label="Current Password"
+                                    type="password"
+                                    value={currentPassword}
+                                    onChange={handleChangeCurrentPassword}
+                                />
+                                <TextField
+                                    label="New Password"
+                                    type="password"
+                                    value={newPassword}
+                                    onChange={handleChangeNewPassowrd}
+                                />
+                                <TextField
+                                    label="Confirm New Password"
+                                    type="password"
+                                    value={newPasswordConfirm}
+                                    onChange={handleChangeNewPassowrdConfirm}
+                                />
+                                <Button onClick = {() => onCancelClick("password") }className = "modifyButton" variant="contained" >  Cancel  </Button>
+
+
+                            </div>
+                        }
+                    </div>
+                    <Button onClick = {(ev) => modifyBtn("password",ev)}  className = "modifyButton" variant="contained" >  {modifyPassword==false? "Modify" : "Save"}  </Button>
+
+                </div> 
+
+            </div>
         </div>
     )
 }
--- src/api/listApi.js
+++ src/api/listApi.js
@@ -205,9 +205,6 @@ const listAPI = {
     const urlSuffix = `user/${
       jwtDecode(localStorage.getItem("token")).id
     }/trip/${data.tripId}/step/${data.idStep}/day`;
-
-    console.log(urlPrefix + urlSuffix);
-
     return fetch(urlPrefix + urlSuffix, {
       method: "GET",
       headers: {
@@ -239,6 +236,23 @@ const listAPI = {
       }),
     }).then((res) => res.json());
   },
+
+  GetUser: () => {
+    const urlSuffix = `user/${
+      jwtDecode(localStorage.getItem("token")).id
+    }`;
+
+    console.log(urlPrefix + urlSuffix);
+
+    return fetch(urlPrefix + urlSuffix, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      }, 
+    }).then((res) => res.json());
+  },
+
 };
 
 export default listAPI;
