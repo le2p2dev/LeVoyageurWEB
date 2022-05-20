bccb8e165f30614cdc154bcfa4700ae5a80752f5
Mathieu Voyer <mathieu900v@hotmail.fr>
2022-05-20 11:54:46 +0200

    [CSS] Modals enhanced

--- src/components/map/DeleteModal.js
+++ src/components/map/DeleteModal.js
@@ -1,16 +1,16 @@
 import React, {useState } from "react";
-import Button from '@mui/material/Button';
+import { InputAdornment, FormControl, InputLabel, Input, Button, MenuItem, TextField, IconButton, Modal } from "@mui/material";
 
 
 const DeleteModal = (props) => {
 
     return(
 
-        <div>
-            <p> Are you sure you want to delete this {props.type} </p>
-            <div className = "BtnBox">
-                <Button onClick = {props.yesBtnFunction} variant="contained"> Yes </Button>
-                <Button onClick = {props.noBtnFunction} variant="contained"> No </Button>
+        <div style={{justifyContent: "center"}}>
+            <p> Are you sure you want to delete this {props.type} ?</p>
+            <div style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly"}}>
+                <Button onClick = {props.yesBtnFunction} style={{backgroundColor: "red"}} variant="contained"> Yes </Button>
+                <Button onClick = {props.noBtnFunction} variant="contained"> Cancel </Button>
             </div>
         </div>
 
