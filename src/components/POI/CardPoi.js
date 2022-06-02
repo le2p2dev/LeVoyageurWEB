f2f3e41a09ea2548ff6e8b8962c253052588f0de
Mathieu Voyer <mathieu900v@hotmail.fr>
2022-06-02 11:08:27 +0200

    [CSS] ListView finished, Responsive and Beautiful

--- src/components/POI/CardPoi.js
+++ src/components/POI/CardPoi.js
@@ -1,23 +1,15 @@
-import {useQuery} from "react-query";
 import {Button, Grid, Switch, TextField} from "@mui/material";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-
-
-import EditIcon from '@mui/icons-material/Edit';
 import React from "react";
-
-
-
 
 const CardPoi = ({id,title,description}) => {
 
-
-    return <Grid item xs={4} >
-      id : {id}
-    </Grid>
+  return <Grid item xs={6} >
+    <p style={{
+      textAlign: "left",
+      paddingLeft: "5%",
+      paddingRight: "3%",
+    }}>- {title ? title : "No title"}</p>
+  </Grid>
 }
 
 export default CardPoi
--- src/components/map/ListView.js
+++ src/components/map/ListView.js
@@ -2,20 +2,10 @@ import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import listAPI from "../../api/listApi";
-import DayList from "../day/DayList";
 import ListDay from "../day/ListDay";
 
 const ListView = () => {
   const { id } = useParams();
-
-  const styles = {
-    border: "solid 3px black",
-    borderRadius: "15px",
-    width: "92vw",
-    height: "80vh",
-    overflow: "hidden",
-    overflowY: "scroll",
-  };
 
   const { isLoading, data: Steps } = useQuery(id + "POIs", () =>
     listAPI.GetStepsFromTrip(id)
@@ -27,21 +17,26 @@ const ListView = () => {
 
   return (
     <>
-      <div style={styles}>
-        <h3> List View for steps</h3>
+      <div style={{
+            width: "95vw",
+            height: "90vh",
+            overflow: "hidden",
+            overflowY: "scroll",}}>
+        <h3 style={{paddingLeft: "1%",}}>List View</h3>
         {Steps.map((step, no) => {
           return (
             <div
               key={step.id}
               style={{
-                margin: "10px",
-                padding: "10px",
+                margin: "1%",
+                padding: "1%",
+                borderRadius: "5px",
                 boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
               }}
             >
               {step.title ? (
                 <h3>
-                  {no}. {step.title}
+                  Step {no} : {step.title}
                 </h3>
               ) : (
                 <h3>{no}. no title</h3>
