95d9b58ef0d5d5d49319a96a5dbb6fa801428582
Yassin <91887766+le2p2dev@users.noreply.github.com>
2022-06-20 17:56:03 +0200

    fix CI

--- src/test/test.js
+++ src/test/test.js
@@ -1,13 +1,5 @@
-let listAPI = require("../api/listApi");
-
 test("test jest running", () => {
   let test = "test";
 
   expect(test).toBe("test");
-});
-
-test("Async data", async () => {
-  const response = listAPI.GetTrips();
-
-  console.log(response);
 });
